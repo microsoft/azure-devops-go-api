a71be1cd4ec03fbeb47262a5b372033bd33bf277
Ted Chambers <tedchamb@microsoft.com>
2019-10-11 11:38:24 -0400

    add a few missing parameters

--- azuredevops/git/client.go
+++ azuredevops/git/client.go
@@ -3436,8 +3436,15 @@ func (client *ClientImpl) GetPullRequestCommits(ctx context.Context, args GetPul
 	}
 	routeValues["pullRequestId"] = strconv.Itoa(*args.PullRequestId)
 
+	queryParams := url.Values{}
+	if args.Top != nil {
+		queryParams.Add("$top", strconv.Itoa(*args.Top))
+	}
+	if args.ContinuationToken != nil {
+		queryParams.Add("continuationToken", *args.ContinuationToken)
+	}
 	locationId, _ := uuid.Parse("52823034-34a8-4576-922c-8d8b77e9e4c4")
-	resp, err := client.Client.Send(ctx, http.MethodGet, locationId, "5.1", routeValues, nil, nil, "", "application/json", nil)
+	resp, err := client.Client.Send(ctx, http.MethodGet, locationId, "5.1", routeValues, queryParams, nil, "", "application/json", nil)
 	if err != nil {
 		return nil, err
 	}
@@ -3456,6 +3463,10 @@ type GetPullRequestCommitsArgs struct {
 	PullRequestId *int
 	// (optional) Project ID or project name
 	Project *string
+	// (optional) Maximum number of commits to return.
+	Top *int
+	// (optional) The continuation token used for pagination.
+	ContinuationToken *string
 }
 
 // Return type for the GetPullRequestCommits function
